280aa51d0a9f9a18ccb0068a697210cd468c576b
Arnaud Delorme <arnodelorme@gmail.com>
2024-10-02 09:57:02 -0700

    minor edits

--- pop_resample.py
+++ pop_resample.py
@@ -6,11 +6,11 @@ from pop_loadset import pop_loadset
 from pop_saveset import pop_saveset
 from oct2py import octave as eeglab
 
-def pop_resample( EEG, freq):
+def pop_resample( EEG, freq): # 2 additional parameters in MATLAB (never used)
     
-    pop_saveset(EEG, 'tmp.set')
+    pop_saveset(EEG, 'tmp.set') # 0.8 seconds
         
-    path2eeglab = '/System/Volumes/Data/data/matlab/eeglab/'
+    path2eeglab = '/System/Volumes/Data/data/matlab/eeglab/' # init >10 seconds
     eeglab.addpath(path2eeglab + '/functions/guifunc')
     eeglab.addpath(path2eeglab + '/functions/popfunc')
     eeglab.addpath(path2eeglab + '/functions/adminfunc')
@@ -19,11 +19,11 @@ def pop_resample( EEG, freq):
     eeglab.addpath(path2eeglab + '/functions/miscfunc')
     eeglab.addpath(path2eeglab + '/plugins/dipfit')
 
-    EEG2 = eeglab.pop_loadset('tmp.set')
-    EEG2 = eeglab.pop_resample(EEG2, freq)
-    eeglab.pop_saveset(EEG2, 'tmp2.set')
+    EEG2 = eeglab.pop_loadset('tmp.set') # 2 seconds
+    EEG2 = eeglab.pop_resample(EEG2, freq) # 2.4 seconds
+    eeglab.pop_saveset(EEG2, 'tmp2.set') # 2.4 seconds
 
-    EEG3 = pop_loadset('tmp2.set')
+    EEG3 = pop_loadset('tmp2.set') # 0.2 seconds
     
     # delete temporary files
     os.remove('tmp.set')
