482cfa477607f19a1db2e68eb25872e4cc60a578
Christian Kothe <christiankothe@gmail.com>
2025-04-04 11:21:29 -0700

    MatlabWrapper: use different temp-file logic for Windows compatibility and ensure .fdt files also get deleted

--- src/eegprep/eeglabcompat.py
+++ src/eegprep/eeglabcompat.py
@@ -14,6 +14,11 @@ logger = logging.getLogger(__name__)
 # can be either 'OCT' (for Oct2Py) or 'MAT' (MATLAB engine)
 default_runtime = 'OCT'
 
+# directory where temporary .set files are written
+temp_dir = os.path.abspath(os.path.dirname(__file__) + '../../../temp')
+if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir, exist_ok=True)
+
 
 class MatlabWrapper:
     """MATLAB engine wrapper that round-trips calls involving the EEGLAB data structure through files."""
@@ -39,16 +44,26 @@ class MatlabWrapper:
                     break
             if needs_roundtrip:
                 # passage data through a file
-                with tempfile.NamedTemporaryFile(delete=True, suffix='.set') as temp_file:
-                    pop_saveset(args[0], temp_file.name)
+                try:
+                    temp_filename = tempfile.mktemp(dir=temp_dir, suffix='.set')
+                    pop_saveset(args[0], temp_filename)
                     # needs to use eval since returning struct arrays is not supported
-                    self.engine.eval(f"EEG = pop_loadset('{temp_file.name}');", nargout=0)
+                    self.engine.eval(f"EEG = pop_loadset('{temp_filename}');", nargout=0)
                     # TODO: marshalling of extra arguments should follow octave conventions
                     eval_str = f"EEG = {name}(EEG{',' if args[1:] else ''}{','.join([self.marshal(a) for a in args[1:]])});"
                     print(eval_str)
                     self.engine.eval(eval_str, nargout=0)
-                    self.engine.eval(f"pop_saveset(EEG, '{temp_file.name}');", nargout=0)
-                    return pop_loadset(temp_file.name)
+                    self.engine.eval(f"pop_saveset(EEG, '{temp_filename}');", nargout=0)
+                    return pop_loadset(temp_filename)
+                finally:
+                    # delete temporary file
+                    try:
+                        # noinspection PyUnboundLocalVariable
+                        os.remove(temp_filename)
+                        if os.path.exists(fdt_file := temp_filename.replace('.set', '.fdt')):
+                            os.remove(fdt_file)
+                    except OSError as e:
+                        logger.warning(f"Error deleting temporary file {temp_filename}: {e}")
             else:
                 # run it directly
                 return getattr(self.engine, name)(*args)
@@ -71,7 +86,7 @@ class OctaveWrapper:
                     break
             if needs_roundtrip:
                 # passage data through a file
-                with tempfile.NamedTemporaryFile(delete=False, suffix='.set') as temp_file:
+                with tempfile.NamedTemporaryFile(dir=temp_dir, delete=False, suffix='.set') as temp_file:
                     pop_saveset(args[0], temp_file.name)
                     # needs to use eval since returning struct arrays is not supported
                     self.engine.eval(f"EEG = pop_loadset('{temp_file.name}');", nargout=0)
