1f431b2330e5fd16810b2902fe41e79a071cb3b5
Arnaud Delorme <arnodelorme@gmail.com>
2025-08-21 22:48:53 -0700

    fix test cases eeg_findboundaries

--- tests/test_eegfindboundaries.py
+++ tests/test_eegfindboundaries.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from eegprep.eeglabcompat import get_eeglab
-from eegprep.eeg_findboundaries import eeg_findboundaries as py_eeg_findboundaries
+from eegprep.eeg_findboundaries import eeg_findboundaries
 
 
 # class TestEegFindBoundariesParity(unittest.TestCase):
@@ -15,37 +15,39 @@ from eegprep.eeg_findboundaries import eeg_findboundaries as py_eeg_findboundari
 #         EEG = {
 #             'setname': 'testset',
 #             'event': [
-#                 {'type': 'boundary'},
-#                 {'type': 'stim'},
-#                 {'type': 'boundary123'},
-#                 {'type': 'resp'},
+#                 {'type': 'boundary', 'latency': 0},
+#                 {'type': 'stim', 'latency': 20},
+#                 {'type': 'boundary123', 'latency': 30},
+#                 {'type': 'resp', 'latency': 40},
 #             ]
 #         }
-#         py_out = py_eeg_findboundaries(EEG=EEG)
+#         py_out = eeg_findboundaries(EEG=EEG)
 #         ml_out = self.eeglab.eeg_findboundaries(EEG)
-#         self.assertTrue(np.array_equal(np.array(py_out), np.array(ml_out)))
+#         # MATLAB returns column vector, Python returns list - compare flattened versions
+#         self.assertTrue(np.array_equal(np.array(py_out), ml_out.flatten()-1))
 
-#     def test_parity_string_types_eventlist(self):
-#         tmpevent = [
-#             {'type': 'boundary'},
-#             {'type': 'foo'},
-#             {'type': 'bar'},
-#             {'type': 'boundary_something'},
-#         ]
-#         py_out = py_eeg_findboundaries(EEG=tmpevent)
-#         ml_out = self.eeglab.eeg_findboundaries(tmpevent)
-#         self.assertTrue(np.array_equal(np.array(py_out), np.array(ml_out)))
+    # def test_parity_string_types_eventlist(self):
+    #     tmpevent = [
+    #         {'type': 'boundary'},
+    #         {'type': 'foo'},
+    #         {'type': 'bar'},
+    #         {'type': 'boundary_something'},
+    #     ]
+    #     py_out = py_eeg_findboundaries(EEG=tmpevent)
+    #     ml_out = self.eeglab.eeg_findboundaries(tmpevent)
+    #     # MATLAB returns column vector, Python returns list - compare flattened versions
+    #     self.assertTrue(np.array_equal(np.array(py_out), ml_out.flatten()))
 
 
 class TestEegFindBoundariesFunctional(unittest.TestCase):
 
     def test_returns_empty_on_empty_input(self):
-        self.assertEqual(py_eeg_findboundaries(EEG={}), [])
-        self.assertEqual(py_eeg_findboundaries(EEG=[]), [])
+        self.assertEqual(eeg_findboundaries(EEG={}), [])
+        self.assertEqual(eeg_findboundaries(EEG=[]), [])
 
     def test_missing_type_field(self):
         tmpevent = [{'latency': 1.0}]
-        self.assertEqual(py_eeg_findboundaries(EEG=tmpevent), [])
+        self.assertEqual(eeg_findboundaries(EEG=tmpevent), [])
 
     def test_string_match_prefix(self):
         tmpevent = [
@@ -54,7 +56,7 @@ class TestEegFindBoundariesFunctional(unittest.TestCase):
             {'type': 'notboundary'},
             {'type': 'BOUNDARY'},  # case sensitive like MATLAB
         ]
-        out = py_eeg_findboundaries(EEG=tmpevent)
+        out = eeg_findboundaries(EEG=tmpevent)
         self.assertEqual(out, [0, 1])
 
     def test_numeric_option_boundary99_true(self):
@@ -64,7 +66,7 @@ class TestEegFindBoundariesFunctional(unittest.TestCase):
         EEG_OPTIONS['option_boundary99'] = 1  # Use 1 to match MATLAB convention
         try:
             tmpevent = [{'type': -99}, {'type': 1}, {'type': -99}]
-            out = py_eeg_findboundaries(EEG=tmpevent)
+            out = eeg_findboundaries(EEG=tmpevent)
             self.assertEqual(out, [0, 2])
         finally:
             EEG_OPTIONS['option_boundary99'] = old
@@ -76,7 +78,7 @@ class TestEegFindBoundariesFunctional(unittest.TestCase):
         EEG_OPTIONS['option_boundary99'] = 0  # Use 0 to match MATLAB convention
         try:
             tmpevent = [{'type': -99}, {'type': 1}, {'type': -99}]
-            out = py_eeg_findboundaries(EEG=tmpevent)
+            out = eeg_findboundaries(EEG=tmpevent)   
             self.assertEqual(out, [])
         finally:
             EEG_OPTIONS['option_boundary99'] = old
@@ -86,9 +88,11 @@ class TestEegFindBoundariesFunctional(unittest.TestCase):
             'setname': 'test',
             'event': [{'type': 'stim'}, {'type': 'boundary'}]
         }
-        out_struct = py_eeg_findboundaries(EEG=EEG)
-        out_list = py_eeg_findboundaries(EEG=EEG['event'])
+        out_struct = eeg_findboundaries(EEG=EEG)
+        out_list = eeg_findboundaries(EEG=EEG['event'])
         self.assertEqual(out_struct, out_list)
+        # Should find 'boundary' at index 1 (0-based)
+        self.assertEqual(out_struct, [1])
 
 
 if __name__ == '__main__':
