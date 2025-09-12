d519ac196788c2d3a4de1497d38a66f5016b8afd
chkothe <christiankothe@gmail.com>
2025-09-12 13:07:29 -0700

    updated test_bids_preproc to automated stress test

--- tests/test_bids_preproc.py
+++ tests/test_bids_preproc.py
@@ -1,38 +1,60 @@
+"""
+Test suite for clean_artifacts.py - All-in-one artifact removal.
 
+This module tests the clean_artifacts function that provides comprehensive
+artifact removal including flatline channels, drifts, noisy channels, bursts, and windows.
+"""
 
-if __name__ == "__main__":
-    import os
-    import sys
-    from eegprep import pop_load_frombids, bids_list_eeg_files, pop_loadset, bids_preproc
+import logging
+import unittest
+import sys
+import socket
+import numpy as np
+import tempfile
+import os
+import shutil
 
+logger = logging.getLogger(__name__)
 
-    if True:
-        # no events
-        fn = '/home/arno/data/OpenNeuro/ds005815-download/sub-01/ses-2/eeg/sub-01_ses-2_task-rest2_eeg.vhdr'
+# Add src to path for imports
+sys.path.insert(0, 'src')
+from eegprep.clean_artifacts import clean_artifacts
+from eegprep.utils.testing import DebuggableTestCase
 
-        # this dataset does not have channel locations
-        fn = '/home/christian/data/OpenNeuro/ds006018-download/sub-002/eeg/sub-002_task-visualoddball_eeg.vhdr'
+curhost = socket.gethostname()
+run_on_hosts = ['ck-carbon']
 
-        # giant files that need to be resampled
-        fn = '/home/christian/data/OpenNeuro/ds004517-download/sub-01/eeg/sub-01_task-eeg_eeg.bdf'
+@unittest.skipIf(curhost not in run_on_hosts, f"Slow stress test skipped by default on hosts other than {run_on_hosts}")
+class TestBidsPreprocSlow(DebuggableTestCase):
+    """Basic test cases for clean_artifacts function."""
 
-        # file with associated electrode locations (but very large file!)
-        fn = '/Users/arno/Python/eegprep/src/eegprep/eeglab/sample_data/test_data/BIDS_test/sub-S1/ses-01/eeg/sub-S1_ses-01_task-Experiment_run-01_eeg.edf'
-        EEG = pop_load_frombids(fn, apply_bids_events=True)
+    def setUp(self):
+        """Set up test fixtures."""
+        if curhost == 'ck-carbon':
+            self.root_path = os.path.expanduser('~/data/OpenNeuro')
+        else:
+            self.root_path = None
+            logger.warning(f"Skipping test TestBidsPreprocSlow on unknown test host {host}; "
+                           f"please add support for your hostname to the above list to "
+                           f"enable this test.")
 
-    if True:
+    def test_crashability(self):
+        """Test basic preproc, first k recordings in all OpenNeuro folders."""
+        from eegprep import bids_preproc
+        all_paths = sorted([d for d in os.listdir(self.root_path) if d.endswith('-download')])
+        print(f"Found {len(all_paths)} BIDS datasets in {self.root_path}:")
+        for p in all_paths:
+            print(f" - {p}")
 
-        # good test case for coord import
-        # rt = '/home/christian/data/OpenNeuro/ds004324-download'
-
-        # good test case for coord inference
-        rt = '/System/Volumes/Data/data/data/STUDIES/ds003061'
-        #/home/christian/data/OpenNeuro/ds005815-download'
-
-        print(f'parsing BIDS files in {rt}...')
-        filelist = bids_list_eeg_files(rt)
-        for fn in filelist:
-            print(' - ' + fn)
-
-        reserve = '' #'1CPU,4GB-5GB,1max' # ''
-        bids_preproc(rt, ReservePerJob=reserve, WithICLabel=True, SkipIfPresent=False)
+        for p in all_paths:
+            bids_preproc(
+                os.path.join(self.root_path, p),
+                # process just the first 2 subjects/sessions, across all runs/tasks
+                subjects=[0,1], sessions=[0,1],
+                SkipIfPresent=True, # <- for quicker re-runs
+                # maximal settings enabled to test everything that could go wrong
+                # (except ICA/IClabel, which are too slow for a test)
+                bidsevent=True,
+                SamplingRate=128,
+                WithInterp=True, EpochEvents=[], EpochLimits=[-0.2, 0.5], EpochBaseline=[None, 0],
+                MinimizeDiskUsage=False)
