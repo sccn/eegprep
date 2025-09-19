82f9df498f0f24136b833309bb7c4683c3e2a945
chkothe <christiankothe@gmail.com>
2025-09-19 15:52:04 -0700

    bids_pipeline.m: added remaining stages

--- scripts/bids_pipeline.m
+++ scripts/bids_pipeline.m
@@ -1,10 +1,42 @@
-
+function ALLEEG = bids_pipeline(rootpath)
 % --- this pipeline is used to run the MATLAB side of the test_bids_preproc() unit test ---
+
+if nargin < 1
+    rootpath = '/home/christian/data/OpenNeuro/ds003061-download'; end
 
 % import BIDS
 [STUDY, ALLEEG] = pop_importbids(...    
-    '/home/christian/data/OpenNeuro/ds003061-download', ...
+    rootpath, ...
     'subjects', {'sub-001','sub-002'}, ...
-    'runs', {'run-001'})
+    'runs', {'1'})
 
+
+for idx=1:length(ALLEEG)
+    EEG = ALLEEG(idx);
+    orig_chanlocs = EEG.chanlocs;
+
+    % resampling
+    EEG = pop_resample(EEG, 128);
+
+    % artifact removal
+    EEG = clean_artifacts(EEG);
+
+    % PICARD
+    EEG = pop_runica(EEG, 'icatype', 'picard');
+
+    % ICLabel
+    EEG = pop_iclabel(EEG, 'Default');
+
+    % reinterpolate channels
+    EEG = eeg_interp(EEG, orig_chanlocs);
+
+    % epoching
+    EEG = pop_epoch(EEG, {}, [-0.2, 0.5]);
+
+    % baseline removal
+    EEG = pop_rmbase(EEG, [-0.2, 0]);
+
+    ALLEEG{idx} = EEG;
+    
+end
 
