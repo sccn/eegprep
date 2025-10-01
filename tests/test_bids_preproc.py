7c112f08245241b9233d18273d50e6811efd1066
chkothe <christiankothe@gmail.com>
2025-10-01 16:53:50 -0700

    test_bids_preproc.py: defined second study to test

--- tests/test_bids_preproc.py
+++ tests/test_bids_preproc.py
@@ -41,82 +41,93 @@ class TestBidsPreproc(DebuggableTestCase):
                            f"please add support for your hostname to the above list to "
                            f"enable this test.")
 
-        # the study that we want to analyze
-        self.test_study = 'ds003061'
+        # list of studies to run end-to-end tests on (set to run first 2 recordings in each)
+        self.studies = [
+            {
+                'studyname': 'ds003061',
+                'subjects': ['001', '002'],
+                'runs': [1],
+            },
+            {
+                'studyname': 'ds002680',
+                'subjects': ['002'],  # first subject, has 2 sessions
+                'runs': [10], # needs to be >= 10 otherwise MATLAB-side filtering by run fails
+            }
+        ]
 
     def test_end2end(self):
         """End-to-end test vs MATLAB."""
-
-        # subset of subjects/runs to compare
-        subjects = [0, 1]
-        runs = [1]
-        # compare the first k trials of each recording
-        max_trials = 30
-        # 1 nV - would ideally be better, but errors compound across multiple preproc
-        # steps amid intermittent single-precision downcasts in original MATLAB code
-        abstol = 1e-3
-
         from eegprep import bids_preproc, pop_loadset, eeg_checkset_strict_mode
         from eegprep.eeglabcompat import get_eeglab
-        from eegprep.eeg_compare import eeg_compare
 
-        if self.root_path is None:
-            self.skipTest("Skipping test_end2end on unknown host")
+        for study in self.studies:
+            # subset of subjects/runs to compare
+            studyname = study['studyname']
+            subjects = study['subjects']
+            runs = study['runs']
+            # compare the first k trials of each recording
+            max_trials = 30
+            # 1 nV - would ideally be better, but errors compound across multiple preproc
+            # steps amid intermittent single-precision downcasts in original MATLAB code
+            abstol = 1e-3
 
-        candidates = [self.test_study, self.test_study + '-download']
-        retain = [d for d in os.listdir(self.root_path) if d in candidates]
-        if len(retain) == 0:
-            self.skipTest(f"Skipping test_end2end because neither {candidates} exist in {self.root_path}")
+            if self.root_path is None:
+                self.skipTest("Skipping test_end2end on unknown host")
 
-        study_path = os.path.join(self.root_path, retain[0])
-        print(f"Running bids_preproc() on {study_path}...")
-        ALLEEG_py = bids_preproc(
-            study_path,
-            ReservePerJob=reservation,
-            # just the first few subjects of the main task
-            Subjects=subjects, Runs=runs,
-            # reuse results for for quicker re-runs
-            SkipIfPresent=True, UseHashes=True, MinimizeDiskUsage=False,
-            # parse events from BIDS, use value column
-            ApplyEvents=True, EventColumn='value', # <- needed for this study to match pop_importbids() in MATLAB
-            # resample
-            SamplingRate=128,
-            # reinterpolate
-            WithInterp=True,
-            # epoch around all events; short limits to reduce disk space
-            EpochEvents=[], EpochLimits=[-0.2, 0.5], EpochBaseline=[-0.2, 0],
-            # temporarily disabled for quicker runs
-            WithPicard=True, WithICLabel=True,
-            # return so we can compare things
-            ReturnData=True)
+            candidates = [studyname, studyname + '-download']
+            retain = [d for d in os.listdir(self.root_path) if d in candidates]
+            if len(retain) == 0:
+                self.skipTest(f"Skipping test_end2end because neither {candidates} exist in {self.root_path}")
 
-        print(f"Running bids_pipeline() on {study_path}...")
-        eeglab = get_eeglab('MATLAB')
-        result_paths = eeglab.bids_pipeline(
-            study_path,
-            [f'sub-{i+1:03d}' for i in subjects],
-            [f'{r}' for r in runs])
+            study_path = os.path.join(self.root_path, retain[0])
+            print(f"Running bids_preproc() on {study_path}...")
+            ALLEEG_py = bids_preproc(
+                study_path,
+                ReservePerJob=reservation,
+                # just the first few subjects of the main task
+                Subjects=subjects, Runs=runs,
+                # reuse results for for quicker re-runs
+                SkipIfPresent=True, UseHashes=True, MinimizeDiskUsage=False,
+                # parse events from BIDS, use value column
+                ApplyEvents=True, EventColumn='value', # <- needed for this study to match pop_importbids() in MATLAB
+                # resample
+                SamplingRate=128,
+                # reinterpolate
+                WithInterp=True,
+                # epoch around all events; short limits to reduce disk space
+                EpochEvents=[], EpochLimits=[-0.2, 0.5], EpochBaseline=[-0.2, 0],
+                # temporarily disabled for quicker runs
+                WithPicard=True, WithICLabel=True,
+                # return so we can compare things
+                ReturnData=True)
 
-        with eeg_checkset_strict_mode(False):
-            ALLEEG_mat = [pop_loadset(p.item()) for p in result_paths.flatten()]
-        for p in result_paths.flatten():
-            p = p.item()
-            if os.path.exists(p):
-                os.remove(p)
+            print(f"Running bids_pipeline() on {study_path}...")
+            eeglab = get_eeglab('MATLAB')
+            result_paths = eeglab.bids_pipeline(
+                study_path,
+                [f'sub-{s}' for s in subjects],
+                [f'{r}' for r in runs])
 
-        # testing up to here because pop_select occasionally retains events on py that
-        # are dropped by the MATLAB code, so things go out of sync at that point
-        print(f"Comparing Python vs MATLAB results (first {max_trials} trials)...")
-        for k in range(min(len(ALLEEG_py), len(ALLEEG_mat))):
-            EEG_py = ALLEEG_py[k]
-            EEG_mat = ALLEEG_mat[k]
-            print(f"Comparing subject #{k}: {EEG_py['filename']}...")
-            np.testing.assert_allclose(EEG_py['data'][:, :, :max_trials],
-                                       EEG_mat['data'][:, :, :max_trials],
-                                       rtol=0, atol=abstol)
-            # PICARD currently doesn't pass its unit test vs MATLAB, so disabling for now
-            # np.testing.assert_allclose(EEG_py['icaweights'], EEG_mat['icaweights'], rtol=0, atol=1e-5)
-            print("passed.")
+            with eeg_checkset_strict_mode(False):
+                ALLEEG_mat = [pop_loadset(p.item()) for p in result_paths.flatten()]
+            for p in result_paths.flatten():
+                p = p.item()
+                if os.path.exists(p):
+                    os.remove(p)
+
+            # testing up to here because pop_select occasionally retains events on py that
+            # are dropped by the MATLAB code, so things go out of sync at that point
+            print(f"Comparing Python vs MATLAB results (first {max_trials} trials)...")
+            for k in range(min(len(ALLEEG_py), len(ALLEEG_mat))):
+                EEG_py = ALLEEG_py[k]
+                EEG_mat = ALLEEG_mat[k]
+                print(f"Comparing subject #{k}: {EEG_py['filename']}...")
+                np.testing.assert_allclose(EEG_py['data'][:, :, :max_trials],
+                                           EEG_mat['data'][:, :, :max_trials],
+                                           rtol=0, atol=abstol)
+                # PICARD currently doesn't pass its unit test vs MATLAB, so disabling for now
+                # np.testing.assert_allclose(EEG_py['icaweights'], EEG_mat['icaweights'], rtol=0, atol=1e-5)
+                print("passed.")
 
     @unittest.skipIf(curhost not in slow_tests_hosts_only, f"Slow stress test skipped by default on hosts other than {slow_tests_hosts_only}")
     def test_crashability_slow(self):
