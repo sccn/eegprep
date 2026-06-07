e5568ef7dbb93421763360648c0642a0d83f6241
suraj-ranganath <suraj.ranganath@gmail.com>
2026-06-07 03:54:51 -0700

    Preserve statistics package callables

--- src/eegprep/functions/statistics/__init__.py
+++ src/eegprep/functions/statistics/__init__.py
@@ -1,5 +1,29 @@
 """EEGLAB-style statistics helper functions."""
 
+from importlib import import_module
+
+# Import same-name thin modules before binding package callables. Without this,
+# a later ``import eegprep.functions.statistics.fdr`` can replace
+# ``statistics.fdr`` with the submodule object.
+_THIN_MODULES = (
+    "anova1_cell",
+    "anova1rm_cell",
+    "anova2_cell",
+    "anova2rm_cell",
+    "concatdata",
+    "corrcoef_cell",
+    "fdr",
+    "stat_surrogate_ci",
+    "stat_surrogate_pvals",
+    "statcond",
+    "surrogdistrib",
+    "teststat",
+    "ttest2_cell",
+    "ttest_cell",
+)
+for _module_name in _THIN_MODULES:
+    import_module(f"{__name__}.{_module_name}")
+
 from eegprep.functions.statistics._core import (
     ConcatenatedData,
     FDRResult,
@@ -22,6 +46,8 @@ from eegprep.functions.statistics._core import (
     ttest2_cell,
     ttest_cell,
 )
+
+del import_module, _module_name, _THIN_MODULES
 
 __all__ = [
     "ConcatenatedData",
